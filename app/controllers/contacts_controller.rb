44fe48499e308153577c5f414b4a7e01db59f138
Grace Drone <gsdrone@gmail.com>
2015-08-17 21:09:53 +0000

    Add form validation and flash messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,20 @@ class ContactsController < ApplicationController
     end
     
     def create
+      @contact = Contact.new(contact_params)
+      
+      if @contact.save
+        flash[:success] = "Message sent! We'll be in touch shortly."
+        redirect_to new_contact_path 
+      else
+        flash[:danger] = "Error, message not sent."
+        redirect_to new_contact_path 
+      end
+      
     end
+    
+    private
+      def contact_params
+        params.require(:contact).permit(:name, :email, :comments) #white list attributes for contact object for security
+      end
 end
