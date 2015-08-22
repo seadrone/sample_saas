a42dbef617d39d8191bbcc817a26931d68f79865
Grace Drone <gsdrone@gmail.com>
2015-08-22 23:20:30 +0000

    Update notice styles, still need to add notice to its own row class

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   
+  before_filter :select_plan, only: :new
+  
   def create
     super do |resource|
       if params[:plan]
@@ -11,7 +13,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
         end
       end
     end
-    
   end
+  
+  private
+    def select_plan
+      unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+      flash[:notice] = "Please select a basic or pro membership plan."
+      redirect_to root_url
+    end
   
 end
