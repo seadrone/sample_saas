add5339c8c918573a1e201b6d6aab7f08ad73c10
Grace Drone <gsdrone@gmail.com>
2015-09-06 22:32:16 +0000

    Add profiles new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,7 @@
 class ProfilesController < ApplicationController
+  def new
+    #form for users to fill out their own profile
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile
+  end
 end
